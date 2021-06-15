5681ff371c0f9eea62152853ea33aaafb91ee493
Andres Yebra <andres.yebra@timbox.com.mx>
2021-06-14 21:17:43 -0500

    Generating the RealState model and tests model

---
+++ db/migrate/20210615021532_create_real_estates.rb
@@ -0,0 +1,20 @@
+class CreateRealEstates < ActiveRecord::Migration[6.1]
+  def change
+    create_table :real_estates do |t|
+      t.integer :id
+      t.string :name
+      t.integer :type
+      t.string :street
+      t.string :external_number
+      t.string :internal_number
+      t.string :neighborhood
+      t.string :city
+      t.string :country
+      t.integer :rooms
+      t.integer :bathrooms
+      t.string :comments
+
+      t.timestamps
+    end
+  end
+end
