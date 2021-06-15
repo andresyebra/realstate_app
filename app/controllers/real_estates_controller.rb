7ac85351e17a0b153de0d7512c6cf2b049340335
Andres Yebra <andres.yebra@timbox.com.mx>
2021-06-15 00:20:14 -0500

    Enable https methods for real_estate_controller

--- app/controllers/real_estates_controller.rb
+++ app/controllers/real_estates_controller.rb
@@ -3,56 +3,69 @@ class RealEstatesController < ApplicationController
 
   # GET /real_estates
   def index
-    @real_estates = RealEstate.all
+    begin
+      @real_estates = RealEstate.all.select(:id, :name, :type, :city, :country)
+      render :json => @real_estates, status: 200
+    rescue Exception => e
+      render json: { error: "Error:#{e}.", status: 400 }, status: 400
+    end
   end
 
   # GET /real_estates/1
   def show
-  end
-
-  # GET /real_estates/new
-  def new
-    @real_estate = RealEstate.new
-  end
-
-  # GET /real_estates/1/edit
-  def edit
+    begin
+      render json: @real_estate, status: 200
+    rescue Exception => e
+      render json: { error: "Error:#{e}.", status: 400 }, status: 400
+    end
   end
 
   # POST /real_estates
   def create
-    @real_estate = RealEstate.new(real_estate_params)
-
-    if @real_estate.save
-      redirect_to @real_estate, notice: 'Real estate was successfully created.'
-    else
-      render :new
+    begin
+        @real_estate = RealEstate.new(real_estate_params)
+        if @real_estate.save
+          render json: @real_estate, status: 201
+        else
+          render json: { error: "Error:#{@real_estate.errors}.", status: 400 }, status: 400
+        end
+    rescue Exception => e
+      render json: { error: "Error:#{e}.", status: 400 }, status: 400
     end
+
   end
 
   # PATCH/PUT /real_estates/1
   def update
-    if @real_estate.update(real_estate_params)
-      redirect_to @real_estate, notice: 'Real estate was successfully updated.'
-    else
-      render :edit
+    begin
+      if @real_estate.update(real_estate_params)
+        render json: @real_estate, status: 201
+      else
+        render json: @real_estate.errors, status: 400
+      end
+    rescue Exception => e
+      render json: { error: "Error:#{e}.", status: 400 }, status: 400
     end
   end
 
   # DELETE /real_estates/1
   def destroy
-    @real_estate.destroy
-    redirect_to real_estates_url, notice: 'Real estate was successfully destroyed.'
+    begin
+      @real_estate.destroy
+      render json: @real_estate, status: 201
+    rescue Exception => e
+      render json: { error: "Error: Real estate was not destroyed.", status: 400 }, status: 400
+    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_real_estate
-      @real_estate = RealEstate.find(params[:id])
+      @real_estate = RealEstate.find_by_id(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def real_estate_params
-      params.fetch(:real_estate, {})
+      params.require(:real_estate).permit(:name, :type, :street, :external_number, :internal_number, :neighborhood, :city, :country, :rooms, :bathrooms, :comments)
     end
 end
--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -1,2 +1,8 @@
 class RealEstate < ApplicationRecord
+  # Set type disabled inheritance column
+  self.inheritance_column = :_type_disabled
+  # Set primary key
+  self.primary_key = :id
+  # Set values of type column
+  enum type: [ :house, :department, :land, :commercial_ground ]
 end
